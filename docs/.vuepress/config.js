51c04f12a89b07687fbc1446f0f205d4cae02bbe
Sonia Boldyreva <87830789+sboldyreva@users.noreply.github.com>
2022-02-25 17:27:32 +0300

    PowerPC Stable Release Notes (#122)
    * PowerPC Stable Release Notes
    
    * PowerPC Stable Release Notes
    
    * PowerPC Stable Release Notes
    
    Co-authored-by: Sofia Boldyreva <tysytylalaia@yandex.ru>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,7 @@ module.exports = {
       {
         title: 'Release Notes',
         children: [
+          '/release-notes/8.5-ppc',
           '/release-notes/8.5',
           '/release-notes/8.5-beta-ppc',
           '/release-notes/8.5-beta',
