05c331af984751be72c9180d986e2dfe677d2b9b
Sonia Boldyreva <87830789+sboldyreva@users.noreply.github.com>
2023-02-09 22:27:18 +0100

    Install guide (#230)
    * Added AlmaLinux Installation Guide
    
    * Added AlmaLinux Installation Guide
    
    * Added AlmaLinux Installation Guide
    
    ---------
    
    Co-authored-by: Sofia Boldyreva <tysytylalaia@yandex.ru>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -43,6 +43,7 @@ module.exports = {
       {
         title: 'Documentation',
         children: [
+          '/documentation/installation-guide',
           '/documentation/migration-guide',
           '/documentation/openscap-guide',
           '/documentation/openscap-guide-for-9',
