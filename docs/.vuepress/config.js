ca94d12f061ab8f53611216b9f54b002c6f1e27e
Sonia Boldyreva <87830789+sboldyreva@users.noreply.github.com>
2023-05-11 00:14:56 +0200

    Added 9.2 stable release notes (#261)
    Co-authored-by: Sofia Boldyreva <tysytylalaia@yandex.ru>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,6 +19,7 @@ module.exports = {
         title: 'Release Notes',
         path: '/release-notes/',
         children: [
+          '/release-notes/9.2',
           '/release-notes/9.1',
           '/release-notes/8.7',
           '/release-notes/9.2-beta',
