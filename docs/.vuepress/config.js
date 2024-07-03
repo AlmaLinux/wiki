e96520ba2c81453ffd7d69810c77af13238fd879
benny Vasquez <bennyvasquez@users.noreply.github.com>
2024-07-03 15:12:56 -0400

    Update config.js
    Add sidebar menu item for certification SIG.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,6 +58,7 @@ module.exports = {
 			        ]
 			     },
                 '/sigs/Migration',
+                '/sigs/Certification',
                 '/sigs/ProcessForCreatingNewSIG'
               ]
            },
